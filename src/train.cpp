ce5909bb95943331ed47ab66ce91f7b927c29930
chervyachok3036 <casha13113@gmail.com>
2026-06-07 15:05:59 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,41 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
 #include "train.h"
+
+Train::Train() {
+  first = nullptr;
+  countOp = 0;
+}
+
+void Train::addCar(bool light) {
+  Car* car = new Car;
+  car->light = light;
+  if (!first) {
+    car->next = car;
+    car->prev = car;
+    first = car;
+  } else {
+    Car* last = first->prev;
+    car->next = first;
+    car->prev = last;
+    last->next = car;
+    first->prev = car;
+  }
+}
+
+int Train::getLength() {
+  if (!first) {
+    return 0;
+  }
+  int len = 1;
+  Car* cur = first->next;
+  while (cur != first) {
+    ++len;
+    cur = cur->next;
+    ++countOp;
+  }
+  return len;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
